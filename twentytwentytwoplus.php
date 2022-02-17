6651a4171b8e37d79f9bb8bdbe526c615f9e03e3
nema <40147733+nexnema@users.noreply.github.com>
2022-02-17 15:13:59 +0330

    0.0.2

--- twentytwentytwoplus.php
+++ twentytwentytwoplus.php
@@ -1,17 +1,19 @@
 <?php
 
 /**
- * Plugin Name: TwentyTwentyTwoPlus2
+ * Plugin Name: TwentyTwentyTwoPlus
  * Author: The Resasadr dev team
  * Description: Add twentytwentytwoplus css to theme
- * Version: 0.0.1
+ * Version: 0.0.2
  * Requires at least: 5.8
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
 
+const STYLE_SOURCE = "https://cdn.cleveraj.net/twentytwentytwoplus/style.css";
+
 function add_css(): void
 {
-    wp_enqueue_style('tttpStyle', "https://cdn.cleveraj.net/twentytwentytwoplus/style.css");
+    wp_enqueue_style('tttpStyle', STYLE_SOURCE);
 }
 add_action('wp_enqueue_scripts', 'add_css');
